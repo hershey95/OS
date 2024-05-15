75d0800fb6af5ecf757f947885b3d540bf13481d
hershey95 <89961315+hershey95@users.noreply.github.com>
2024-05-15 23:15:32 +0900

    syn

--- kernel/20140653/pi.c
+++ kernel/20140653/pi.c
@@ -1,13 +1,13 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <math.h>
-#include <unistd.h> // For getpid()
+#include <unistd.h>
+#include <time.h>
 
-#define NUM_THREADS 2
-#define NUM_POINTS 1000000
-#define RADIUS 1
+#define NUM_POINTS 1000000 // 생성할 총 점의 개수
+#define NUM_THREADS 4 // 사용할 스레드의 개수
+#define RADIUS 1.0 // 원의 반지름
 
 long points_in_circle = 0;
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
@@ -17,7 +17,9 @@ void *throw_darts(void *param) {
     unsigned int rand_state = tid + time(NULL);
     long local_points_in_circle = 0;
 
-    for (int i = 0; i < NUM_POINTS/NUM_THREADS; i++) {
+    printf("Thread %d (PID %d): 랜덤 포인트 생성 시작\n", tid, getpid());
+
+    for (int i = 0; i < NUM_POINTS / NUM_THREADS; i++) {
         double x = (double)rand_r(&rand_state) / RAND_MAX * 2 - 1;
         double y = (double)rand_r(&rand_state) / RAND_MAX * 2 - 1;
         if (sqrt(x * x + y * y) <= RADIUS) {
@@ -31,13 +33,15 @@ void *throw_darts(void *param) {
     pthread_mutex_unlock(&mutex);
 
     // Print the thread's results
-    printf("Thread %d (PID %d): 원안에 포인트 %ld \n", tid, getpid(), local_points_in_circle);
+    printf("Thread %d (PID %d): 원안에 포인트 %ld\n", tid, getpid(), local_points_in_circle);
     pthread_exit(0);
 }
 
 int pi() {
     pthread_t threads[NUM_THREADS];
     int thread_args[NUM_THREADS];
+
+    printf("메인 스레드 (PID %d): 프로그램 시작\n", getpid());
 
     // Reset global counter before creating threads
     points_in_circle = 0;
@@ -49,16 +53,19 @@ int pi() {
     for (int i = 0; i < NUM_THREADS; i++) {
         thread_args[i] = i;
         pthread_create(&threads[i], NULL, throw_darts, (void *)&thread_args[i]);
+        printf("메인 스레드 (PID %d): 스레드 %d 생성\n", getpid(), i);
     }
 
     // Wait for the threads to finish
     for (int i = 0; i < NUM_THREADS; i++) {
         pthread_join(threads[i], NULL);
+        printf("메인 스레드 (PID %d): 스레드 %d 종료\n", getpid(), i);
     }
 
     // Calculate and print the estimated value of pi
     double pi_estimate = 4 * (double)points_in_circle / NUM_POINTS;
-    printf("Parent (PID %d): 예측된 pi 값: %f\n", getpid(), pi_estimate);
+    printf("메인 스레드 (PID %d): 예측된 pi 값: %f\n", getpid(), pi_estimate);
 
+    printf("메인 스레드 (PID %d): 프로그램 종료\n", getpid());
     return 0;
 }
